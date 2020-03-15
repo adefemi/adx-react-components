e9fa3ab20005de50223f5a531d0bb7187956c844
Adefemi <adefemi.oseni@algorimsng.com>
2020-03-15 16:50:02 +0100

    added result component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import Pagination from "./components/Pagination/pagination";
+import Result from "./components/Result/result";
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -8,8 +8,16 @@ function App() {
   }, []);
   return (
     <div className="App">
-      <h4>Pagination test</h4>
-      <Pagination defaultCurrent={1} total={500} current={2} />
+      <h4>Result test</h4>
+      <Result
+        // status="error"
+        title="Successfully Purchased Cloud Server ECS!"
+        subTitle="Order number: 2017182818828182881 Cloud server configuration takes 1-5 minutes, please wait."
+        extra={[
+          <button key="console">Go Console</button>,
+          <button key="buy">Buy Again</button>
+        ]}
+      />
     </div>
   );
 }
